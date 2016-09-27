5125a20633b045a96591772a5fabbae2403e4317
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-09-27 14:59:04 +0200

    work

---
+++ any_tensor.cpp
@@ -0,0 +1,80 @@
+#include "any_tensor.hpp"
+
+
+AnyScalar::AnyScalar(const AnyScalar& s) {
+  t = s.t;
+  data_double = s.data_double;
+  data_sx = s.data_sx;
+  data_mx = s.data_mx;
+}
+
+AnyScalar& AnyScalar::operator=(const AnyScalar& s) {
+  t = s.t;
+  data_double = s.data_double;
+  data_sx = s.data_sx;
+  data_mx = s.data_mx;
+  return *this;
+}
+
+AnyScalar::AnyScalar(double s) {
+  t = TENSOR_DOUBLE;
+  data_double = s;
+}
+
+AnyScalar::AnyScalar() {
+  t = TENSOR_NULL;
+  data_double = 0;
+  data_sx = 0;
+  data_mx = 0;
+}
+
+AnyScalar::operator double() {
+  assert(TENSOR_DOUBLE);
+  return data_double;
+}
+
+AnyTensor& AnyTensor::operator=(const AnyTensor& s) {
+  t = s.t;
+  data_double = s.data_double;
+  data_sx = s.data_sx;
+  data_mx = s.data_mx;
+  return *this;
+}
+
+AnyTensor::AnyTensor(const AnyTensor& s) {
+  t = s.t;
+  data_double = s.data_double;
+  data_sx = s.data_sx;
+  data_mx = s.data_mx;
+}
+
+AnyTensor::AnyTensor(const DT & s) {
+  t = TENSOR_DOUBLE;
+  data_double = new DT(s);
+  data_sx = 0;
+  data_mx = 0;
+}
+
+//AnyTensor::AnyTensor(const AnyScalar& s) {
+//  if (s.t == TENSOR_DOUBLE) {
+//
+//  }
+//}
+
+AnyTensor::AnyTensor() {
+  t = TENSOR_NULL;
+  data_double = 0;
+  data_sx = 0;
+  data_mx = 0;
+}
+
+AnyTensor::operator DT() {
+  assert(TENSOR_DOUBLE);
+  return DT(*data_double);
+}
+
+AnyTensor::~AnyTensor() {
+  if (data_double) delete data_double;
+  if (data_sx) delete data_sx;
+  if (data_mx) delete data_mx;
+}
---
+++ any_tensor.hpp
@@ -0,0 +1,40 @@
+#ifndef ANY_TENSOR_HPP_INCLUDE
+#define ANY_TENSOR_HPP_INCLUDE
+
+#include "tensor.hpp"
+
+enum TensorType {TENSOR_NULL, TENSOR_DOUBLE, TENSOR_SX, TENSOR_MX};
+
+class AnyScalar {
+
+  public:
+    AnyScalar& operator=(const AnyScalar&);
+    AnyScalar(const AnyScalar& s);
+    AnyScalar(double s);
+    AnyScalar();
+    operator double();
+  private:
+    TensorType t;
+    double data_double;
+    SX data_sx;
+    MX data_mx;
+};
+
+class AnyTensor {
+  public:
+    AnyTensor& operator=(const AnyTensor&);
+    //AnyTensor(const AnyScalar& s);
+    AnyTensor(const AnyTensor& s);
+    AnyTensor(const DT & t);
+    AnyTensor();
+    ~AnyTensor();
+
+    operator DT();
+  private:
+    TensorType t;
+    DT* data_double;
+    ST* data_sx;
+    MT* data_mx;
+};
+
+#endif
